5a2ebfe0d0b905573d4ab82332cabbf65c948706
Harsh Banka <harshbanka321@gmail.com>
2025-06-25 19:40:55 +0530

    feat: Enhance SEO and localization support with dynamic metadata and structured data integration

---
+++ src/app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/admin/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/admin/'],
+      },
+    ],
+    sitemap: 'https://ducologix.com/sitemap.xml',
+    host: 'https://ducologix.com',
+  }
+}
